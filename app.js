469673b7a4fd9e4582f3f0d6157d32e2aabdd83c
Kealoha1988 <kealohaabe1988@gmail.com>
2021-11-12 21:46:27 -0500

    cleaned final solution

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ module.exports = {
   missingNumber: function (arr) {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i + 1] != arr[i] + 1){
-        return arr[i + 1] - 1
+        return arr[i] + 1
       }
     }
   }
