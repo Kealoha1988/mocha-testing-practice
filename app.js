707ba987bec12d920db9a3fe239bde87281e2a02
Kealoha1988 <kealohaabe1988@gmail.com>
2021-12-10 23:01:27 -0500

    updated targetIndex in app.js

--- app.js
+++ app.js
@@ -215,8 +215,8 @@ module.exports = {
     }
   },
   //Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-  targetIndex: function (nums){
-    
+  targetIndex: function (nums, target){
+
   }
 
 }
