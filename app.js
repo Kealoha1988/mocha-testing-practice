fa358412d886f276aa9a998765f71823af912048
Kealoha1988 <kealohaabe1988@gmail.com>
2021-12-10 23:25:40 -0500

    targetIndex() first test solution

--- app.js
+++ app.js
@@ -216,7 +216,12 @@ module.exports = {
   },
   //Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
   targetIndex: function (nums, target){
-
+    for (let i = 0; i < nums.length; i ++) {
+      if (nums[i] == target){
+        console.log(target)
+        return nums.indexOf(target)
+      }
+    }
   }
 
 }
