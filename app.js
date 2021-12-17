21db5698227d12c0038dbd0526d58a9975aff9e6
Kealoha1988 <kealohaabe1988@gmail.com>
2021-12-17 13:57:20 -0500

    created inorderTraversal

--- app.js
+++ app.js
@@ -224,6 +224,10 @@ module.exports = {
         return nums.indexOf(target - 1) + 1
       }
     }
+  },
+  //Given the root of a binary tree, return the inorder traversal of its nodes' values.
+  inorderTraversal: function (root) {
+    
   }
 
 }
